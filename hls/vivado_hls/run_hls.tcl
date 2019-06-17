9ec99214b1f38f326f69444ac8e7002e7824d517
Marcelo Vicente <marcelo.vicente@cern.ch>
2019-06-17 17:39:20 -0500

    HLS code is now compiled through a makefile for simplicity

--- hls/vivado_hls/run_hls.tcl
+++ hls/vivado_hls/run_hls.tcl
@@ -2,16 +2,18 @@
 # https://github.com/hls-fpga-machine-learning/hls4ml
 
 array set opt {
-    csim   1
-    synth  1
-    cosim  1
-    export 1
-    tv test_in.txt
+    csim   0
+    synth  0
+    cosim  0
+    export 0
+    tv_in  ""
+    tv_out ""
+    tv_ref ""
 }
 
 foreach arg $::argv {
     foreach o [lsort [array names opt]] {
-         regexp "$o=+(\\w+)$" $arg unused opt($o)
+         regexp "$o\=(.*)" $arg unused opt($o)
     }
 }
 
@@ -23,52 +25,62 @@ proc report_time { op_name time_start time_end  } {
     puts "***** ${op_name} COMPLETED IN ${time_h}h${time_m}m${time_s}s *****"
 }
 
-# open the project, don't forget to reset
-open_project -reset proj
+# open the project
+open_project proj
 
-#Add sources and specify top function
+# Add sources and specify top function
 source "./sources.tcl"
 
-#reset the solution
-open_solution -reset "solution1"
+# Open the solution
+open_solution "solution1"
 
-#Specify FPGA and clock constraints
+# Specify FPGA and clock constraints
 source "./solution.tcl"
 
-#Add Vivado HLS project directives
+# Add Vivado HLS project directives
 source "./directives.tcl"
 
 if {$opt(csim)} {
-   puts "***** C SIMULATION *****"
-   set time_start [clock clicks -milliseconds]
-   csim_design -argv "-r $opt(tv) -v"
-   set time_end [clock clicks -milliseconds]
-   report_time "C SIMULATION" $time_start $time_end
+  puts "***** C SIMULATION *****"
+  set time_start [clock clicks -milliseconds]
+  set args "-r $opt(tv_in) -v"
+  if {[string compare $opt(tv_out) ""]} {set args [append args " -w $opt(tv_out)"]}
+  if {[string compare $opt(tv_ref) ""]} {set args [append args " -c $opt(tv_ref)"]}
+  puts "Executing: csim_design -argv \"$args\""
+  csim_design -argv "$args"
+  set time_end [clock clicks -milliseconds]
+  report_time "C SIMULATION" $time_start $time_end
 }
 
 if {$opt(synth)} {
-   puts "***** C/RTL SYNTHESIS *****"
-   set time_start [clock clicks -milliseconds]
-   csynth_design
-   set time_end [clock clicks -milliseconds]
-   report_time "C/RTL SYNTHESIS" $time_start $time_end
-   if {$opt(cosim)} {
-       puts "***** C/RTL SIMULATION *****"
-       set time_start [clock clicks -milliseconds]
-       cosim_design -argv "-r $opt(tv) -v" -trace_level all
-       set time_end [clock clicks -milliseconds]
-       report_time "C/RTL SIMULATION" $time_start $time_end
-   }
-   if {$opt(export)} {
-      puts "***** EXPORT IP *****"
-      set time_start [clock clicks -milliseconds]
-      export_design -format ip_catalog
-      set time_end [clock clicks -milliseconds]
-      report_time "EXPORT IP" $time_start $time_end
+  puts "***** C/RTL SYNTHESIS *****"
+  set time_start [clock clicks -milliseconds]
+  csynth_design
+  set time_end [clock clicks -milliseconds]
+  report_time "C/RTL SYNTHESIS" $time_start $time_end
+}
 
-      puts "***** GENERATING WRAPPERS *****"
-      puts [exec python ../wrapper_generator.py src/algo_top_parameters.h --wrapper ../rtl/algo_top_wrapper.vhd]
-   }
+if {$opt(cosim)} {
+  puts "***** C/RTL SIMULATION *****"
+  set time_start [clock clicks -milliseconds]
+  set args "-r $opt(tv_in) -v"
+  if {[string compare $opt(tv_out) ""]} {set args [append args " -w $opt(tv_out)"]}
+  if {[string compare $opt(tv_ref) ""]} {set args [append args " -c $opt(tv_ref)"]}
+  puts "Executing: cosim_design \"-argv -r $opt(args) -v\" -trace_level all"
+  cosim_design -argv "-r $opt(args) -v" -trace_level all
+  set time_end [clock clicks -milliseconds]
+  report_time "C/RTL SIMULATION" $time_start $time_end
+}
+
+if {$opt(export)} {
+  puts "***** EXPORT IP *****"
+  set time_start [clock clicks -milliseconds]
+  export_design -format ip_catalog
+  set time_end [clock clicks -milliseconds]
+  report_time "EXPORT IP" $time_start $time_end
+
+  puts "***** GENERATING WRAPPERS *****"
+  puts [exec python ../wrapper_generator.py src/algo_top_parameters.h --wrapper ../rtl/algo_top_wrapper.vhd]
 }
 
 exit
